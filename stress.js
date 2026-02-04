27807ac68ec502e129f8fad4f7c43d52ad829dd7
Vishisht16 <vishishtmishra150@gmail.com>
2026-02-04 07:12:40 +0000

    added k6 script and updated cpu limit for HPA testing

---
+++ stress.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  // Load test plan
+  stages: [
+    { duration: '30s', target: 20 }, // Reach 20 users in the first 15 seconds (Warm-up)
+    { duration: '2m', target: 100 }, // Reach 100 users in the next 1 minute (Load)
+    { duration: '30s', target: 0 },  // Gradually go back to 0 users (Cool-down)
+  ],
+};
+
+export default function () {
+  // Each virtual user makes a GET request to the /buy endpoint
+  http.get('http://104.198.41.6/heavy');
+  sleep(1); // Each user waits 1 second before making the next request
+}
